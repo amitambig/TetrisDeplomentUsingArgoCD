f895217f55988aa483e8730d1f02948d8651b843
amitambig <49871474+amitambig@users.noreply.github.com>
2024-09-13 11:03:33 +0530

    Delete EKS directory

--- EKS/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-eks-state-file-bucket"
-    key = "eks/terraform.tfstate"
-    encrypt = true
-    region = "us-east-1"
-  }
-}
--- EKS/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for instances"
-  type        = string
-  default     = "newKey"
-}
-variable "subnet_cidr" {
-  default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24","10.0.4.0/24"]
-}
